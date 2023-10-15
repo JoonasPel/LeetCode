324773fe8b87c0275186478c6d894007bf144a36
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-10-15 22:56:26 +0300

    Problem 27

---
+++ leetcode27.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+  public static int RemoveElement(int[] nums, int val)
+  {
+    int
+    leftIdx = 0
+    , swapsCount = 0
+    , rightIdx = nums.Length-1;
+    // not O^2
+    while (leftIdx != rightIdx)
+    {
+      if (nums[leftIdx] == val)
+      {
+        while (leftIdx != rightIdx)
+        {
+          if (nums[rightIdx] != val)
+          {
+            (nums[leftIdx], nums[rightIdx]) = (nums[rightIdx], -1);
+            rightIdx--;
+            swapsCount++;
+            break;
+          } else {
+            nums[rightIdx] = -1;
+          }
+          rightIdx--;
+        }
+      }
+      leftIdx++;
+    }
+    return swapsCount;
+  }
+
+  public static void Main()
+  {
+    int[] nums = new int[] {0,1,2,2,3,0,4,2};
+    RemoveElement(nums, val: 2);
+  }
+}
