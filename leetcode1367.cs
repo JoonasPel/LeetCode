adf80c3d7ca806aee8f485a5e799e5f7390da05f
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-10-17 17:40:17 +0300

    LL in Binary Tree. 2 testcases fail for now :(
    2 testcases fail because the current approach has a logical problem where a chain of linked list validation can be broken and it should be restarted from higher treenode. Might be fixable, not sure.

---
+++ leetcode1367.cs
@@ -0,0 +1,83 @@
+﻿public class Program
+{
+  public class TreeNode
+  {
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+
+    public TreeNode(int val=0, TreeNode? left=null, TreeNode? right=null)
+    {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public class ListNode
+  {
+    public int val;
+    public ListNode? next;
+    public ListNode(int val, ListNode? next=null)
+    {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public class RecursionKillingException : Exception
+  {
+    public RecursionKillingException()
+    {
+    }
+    public RecursionKillingException(string message)
+      : base(message)
+    {
+    }
+  }
+
+  // remove static for leetcode. And change RecursionKillingException to 
+  // normal Exception.
+  public static bool IsSubPath(ListNode head, TreeNode root) {
+    if (head == null || root == null) return false;
+    bool subPathFound = false;
+    void Visit(ListNode? LLnode, TreeNode? treeNode)
+    {
+      if (LLnode == null)
+      {
+        subPathFound = true;
+        throw new RecursionKillingException("kill recursion");
+      }
+      if (treeNode == null) return;
+      if (treeNode.val == LLnode.val)
+      {
+        Visit(LLnode.next, treeNode.left);
+        Visit(LLnode.next, treeNode.right);
+      }
+      else
+      {
+        if (treeNode.val == head.val)
+        {
+          Visit(head.next, treeNode.left);
+          Visit(head.next, treeNode.right);
+        }
+        else
+        {
+          Visit(head, treeNode.left);
+          Visit(head, treeNode.right);       
+        }
+      }
+    }
+    try 
+    {
+      Visit(head, root);
+    }
+    catch (RecursionKillingException) {};
+    return subPathFound;
+  }
+
+  public static void Main()
+  {
+  }
+
+}
