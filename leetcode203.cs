6b0230e593a1efc8f40aa487f1b2065e5234e7c3
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-10-30 23:25:07 +0200

    Remove Linked List Elements

---
+++ leetcode203.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/*
+QUESTION: 203. Remove Linked List Elements
+
+Given the head of a linked list and an integer val, remove all the nodes of
+the linked list that has Node.val == val, and return the new head.
+
+APPROACH:
+*/
+
+public class Program
+{
+
+  public class ListNode
+  {
+    public int val;
+    public ListNode? next;
+
+    public ListNode(int val, ListNode? next = null)
+    {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  // remove static for leetcode
+  public static ListNode? RemoveElements(ListNode? head, int val)
+  {
+    if (head == null) return head;
+    ListNode? current = head.next;
+    ListNode? prev = head;
+    while (current != null)
+    {
+      if (current.val == val)
+      {
+        prev.next = current.next;
+      }
+      else
+      {
+        prev = current;
+      }
+      current = current.next;
+    }
+    return head.val == val ? head.next : head;
+  }
+
+
+  public static void Main()
+  {
+  }
+
+}
