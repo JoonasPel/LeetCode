0868cf91d9a687e1068f9c912fb1400bfdc6d8fc
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-01-15 03:34:51 +0200

    Weekly380

---
+++ Contests/Weekly380/leetcode3005.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+
+/*
+DIFFICULTY: Easy
+QUESTION: 3005. Count Elements With Maximum Frequency
+Q1 of Weekly Contest 380. I did this during the contest.
+
+You are given an array nums consisting of positive integers.
+
+Return the total frequencies of elements in nums such that those elements all
+have the maximum frequency.
+
+The frequency of an element is the number of occurrences of that element in the
+array.
+
+APPROACH:
+Loop the numbers and save counts to a dictionary. And also the highest freq
+found so far. Afterwards get the sum of all counts that are same as the
+highest freq found.
+*/
+
+public class Solution
+{
+  public int MaxFrequencyElements(int[] nums)
+  {
+    var occurences = new Dictionary<int, int>();
+    int maxFrequency = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+      occurences.TryGetValue(nums[i], out int count);
+      occurences[nums[i]] = count + 1;
+      maxFrequency = Math.Max(maxFrequency, count + 1);
+    }
+    int result = 0;
+    foreach (var num in occurences)
+    {
+      if (num.Value == maxFrequency)
+      {
+        result += num.Value;
+      }
+    }
+    return result;
+  }
+}
---
+++ Contests/Weekly380/leetcode3006.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+
+/*
+DIFFICULTY: Medium
+QUESTION: 3006. Find Beautiful Indices in the Given Array I
+Q2 of Weekly Contest 380. I did this during the contest.
+
+You are given a 0-indexed string s, a string a, a string b, and an integer k.
+
+An index i is beautiful if:
+
+    0 <= i <= s.length - a.length
+    s[i..(i + a.length - 1)] == a
+    There exists an index j such that:
+        0 <= j <= s.length - b.length
+        s[j..(j + b.length - 1)] == b
+        |j - i| <= k
+
+Return the array that contains beautiful indices in sorted order from smallest
+to largest.
+
+APPROACH:
+Loop s and if we find a, then loop the s again to find b. But only from the
+range that is needed to be checked which is from i-k to i+k+1. This range thing
+is because the requirement |j - i| <= k. Meaning basically that the b has to be
+found near enough.
+*/
+
+public class Solution
+{
+  public IList<int> BeautifulIndices(string s, string a, string b, int k)
+  {
+    IList<int> result = new List<int>();
+    for (int i = 0; i < (s.Length - a.Length + 1); i++)
+    {
+      if (s[i..(i + a.Length)] == a)
+      {
+        int startIdx = Math.Max(0, i - k);
+        int endIdx = Math.Min(s.Length - b.Length + 1, i + k + 1);
+        for (int j = startIdx; j < endIdx; j++)
+        {
+          if (s[j..(j + b.Length)] == b && Math.Abs(j - i) <= k)
+          {
+            result.Add(i);
+            break;
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  public static void Main()
+  {
+    Solution app = new Solution();
+
+    var res = app.BeautifulIndices("wfvxfzut", "wfv", "ut", 6);
+
+    foreach (var item in res)
+    {
+      Console.WriteLine(item);
+    }
+  }
+
+}
