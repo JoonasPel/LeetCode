112a243b7e7292b566ad38caf179b1d7664adefa
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-01-17 05:15:08 +0200

    Find Numbers with Even Number of Digits

---
+++ leetcode1295.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+
+/*
+DIFFICULTY: Easy
+QUESTION: 1295. Find Numbers with Even Number of Digits
+
+Given an array nums of integers, return how many of them contain an even number
+of digits.
+
+APPROACH:
+Check if a number has even digits by dividing it by 10 until it is under 10.
+*/
+
+public class Solution
+{
+  public int FindNumbers(int[] nums)
+  {
+    int result = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+      if (EvenDigits(nums[i])) result++;
+    }
+    return result;
+
+    static bool EvenDigits(int num)
+    {
+      int x = 1;
+      while (num >= 10)
+      {
+        x++;
+        num /= 10;
+      }
+      return x % 2 == 0;
+    }
+  }
+}
