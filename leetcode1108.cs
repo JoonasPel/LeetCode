861c652f478999457231d4ba1fbe44e7c73e45f7
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-03-04 20:00:27 +0200

    Defanging an IP Address

---
+++ leetcode1108.cs
@@ -0,0 +1,18 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 1108. Defanging an IP Address
+
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public string DefangIPaddr(string address)
+  {
+    return address.Replace(".", "[.]");
+  }
+}
