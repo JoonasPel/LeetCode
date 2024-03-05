146f0e199f95a700ca2bda924dc88c869bcf3bd1
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-03-05 11:55:43 +0200

    Find Words Containing Character

---
+++ leetcode2942.cs
@@ -0,0 +1,32 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2942. Find Words Containing Character
+
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public IList<int> FindWordsContaining(string[] words, char x)
+  {
+    IList<int> result = new List<int>();
+    for (int i = 0; i < words.Length; i++)
+    {
+      if (words[i].Contains(x))
+      {
+        result.Add(i);
+      }
+    }
+    return result;
+  }
+
+  public static void Main()
+  {
+  }
+}
