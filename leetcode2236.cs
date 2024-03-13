b405308d258935724e76a298204fc93c2563bb1b
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-03-13 18:44:09 +0200

    Root Equals Sum of Children

---
+++ leetcode2236.cs
@@ -0,0 +1,28 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2236. Root Equals Sum of Children
+
+You are given the root of a binary tree that consists of exactly 3 nodes: 
+the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its
+two children, or false otherwise.
+
+INTUITION/APPROACH:
+Trivial
+*/
+
+using Utilities;
+
+public class Solution
+{
+  public bool CheckTree(TreeNode root)
+  {
+    if (root == null) return false;
+    return root.val == root.left.val + root.right.val;
+  }
+
+  public static void Main()
+  {
+  }
+}
