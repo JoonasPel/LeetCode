17e9961a423af59b277b323319b91c85e6eebf6c
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-03-19 23:52:04 +0200

    Number of Common Factors

---
+++ leetcode2427.cs
@@ -0,0 +1,36 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2427. Number of Common Factors
+
+Given two positive integers a and b, return the number of common factors of
+a and b.
+
+An integer x is a common factor of a and b if x divides both a and b.
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+
+  public int CommonFactors(int a, int b)
+  {
+    int commonFactors = 0;
+    int x = Math.Min(a, b);
+    for (int i = 1; i <= x; i++)
+    {
+      if (a % i == 0 && b % i == 0)
+      {
+        commonFactors++;
+      }
+    }
+    return commonFactors;
+  }
+
+  public static void Main()
+  {
+    Solution app = new();
+    app.CommonFactors(12, 6);
+  }
+}
+
