81b5b2e66ef086f12cc51fe19fa28714ba9625e3
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-03-24 01:01:51 +0200

    Hamming Distance

---
+++ leetcode461.cs
@@ -0,0 +1,26 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 461. Hamming Distance
+
+The Hamming distance between two integers is the number of positions at which
+the corresponding bits are different.
+
+Given two integers x and y, return the Hamming distance between them.
+
+INTUITION/APPROACH:
+Take xor of the integers, turn into a binary number (string) and count ones.
+*/
+
+using Utilities;
+
+public class Solution
+{
+  public int HammingDistance(int x, int y)
+  {
+    return Convert.ToString(x ^ y, 2).Count(c => c == '1');
+  }
+
+  public static void Main()
+  {
+  }
+}
