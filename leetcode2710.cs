09b10719b9613952527a36ecb447848c2e20d021
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-03-23 05:43:21 +0200

    Remove Trailing Zeros From a String

---
+++ leetcode2710.cs
@@ -0,0 +1,23 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2710. Remove Trailing Zeros From a String
+
+Given a positive integer num represented as a string, return the integer num
+without trailing zeros as a string.
+
+INTUITION/APPROACH:
+I like C#
+*/
+
+
+public class Solution
+{
+  public string RemoveTrailingZeros(string num)
+  {
+    return num.TrimEnd('0');
+  }
+
+  public static void Main()
+  {
+  }
+}
