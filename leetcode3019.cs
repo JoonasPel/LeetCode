9f7f1b47b22524c8e993aeb64488e0bc138ac518
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-03-23 04:33:20 +0200

    Number of Changing Keys

---
+++ leetcode3019.cs
@@ -0,0 +1,41 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 3019. Number of Changing Keys
+
+You are given a 0-indexed string s typed by a user. Changing a key is defined
+as using a key different from the last used key. For example, s = "ab" has a
+change of a key while s = "bBBb" does not have any.
+
+Return the number of times the user had to change the key.
+
+Note: Modifiers like shift or caps lock won't be counted in changing the key
+that is if a user typed the letter 'a' and then the letter 'A' then it will
+not be considered as a changing of key.
+
+INTUITION/APPROACH:
+Loop the chars and every time the earlier char is different, increase result.
+Turn chars to lower to make this not case sensitive.
+*/
+
+
+public class Solution
+{
+  public int CountKeyChanges(string s)
+  {
+    int result = 0;
+    for (int i = 1; i < s.Length; i++)
+    {
+      if (char.ToLower(s[i - 1]) != char.ToLower(s[i]))
+      {
+        result++;
+      }
+    }
+    return result;
+  }
+
+  public static void Main()
+  {
+    Solution app = new();
+    Console.WriteLine(app.CountKeyChanges("aAbBcCDVD"));
+  }
+}
