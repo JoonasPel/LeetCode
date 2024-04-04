031dbbc8531b3e9d639d3f78f2772085e4cb3922
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-04-05 00:43:46 +0300

    Number of 1 Bits

---
+++ leetcode191.cs
@@ -0,0 +1,22 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 191. Number of 1 Bits
+
+DESCRIPTION:
+Write a function that takes the binary representation of a positive integer and
+returns the number of set bits it has (also known as the Hamming weight).
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public int HammingWeight(int n)
+  {
+    return Convert.ToString(n, 2).Count(c => c == '1');
+  }
+
+  public static void Main()
+  {
+  }
+}
