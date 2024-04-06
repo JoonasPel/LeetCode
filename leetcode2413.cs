b106c63e74958910645fe8f84b5cd2f84f09c760
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-04-06 18:12:47 +0300

    Smallest Even Multiple

---
+++ leetcode2413.cs
@@ -0,0 +1,27 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2413. Smallest Even Multiple
+
+DESCRIPTION:
+Given a positive integer n, return the smallest positive integer that is a
+multiple of both 2 and n. 
+
+INTUITION/APPROACH:
+If n is even, return n, otherwise return 2*n.
+My approach checks if the last bit is one (is yes, num is odd) but I think the
+language/compiler probably does it like that anyway, if writing num % 2 == 0.
+*/
+
+
+public class Solution
+{
+  public int SmallestEvenMultiple(int n)
+  {
+    return (n & 1) == 1 ? 2 * n : n;
+    // return n % 2 != 0 ? n * 2 : n;
+  }
+
+  public static void Main()
+  {
+  }
+}
