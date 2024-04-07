d9e38c4f6cea14cfa6d277a0688c807e07827291
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-04-07 03:32:10 +0300

    Subtract the Product and Sum of Digits of an Integer

---
+++ leetcode1281.cs
@@ -0,0 +1,34 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 1281. Subtract the Product and Sum of Digits of an Integer
+
+DESCRIPTION:
+Given an integer number n, return the difference between the product of its
+digits and the sum of its digits.
+
+INTUITION/APPROACH:
+*/
+
+
+public class Solution
+{
+  public int SubtractProductAndSum(int n)
+  {
+    int product = 1;
+    int sum = 0;
+    while (n != 0)
+    {
+      int remainder = n % 10;
+      product *= remainder;
+      sum += remainder;
+      n /= 10;
+    }
+    return product - sum;
+  }
+
+  public static void Main()
+  {
+    Solution app = new();
+    Console.WriteLine(app.SubtractProductAndSum(234));
+  }
+}
