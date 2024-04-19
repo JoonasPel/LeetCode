3d9e3e24fe9d938fcdf1e440e0188c80904c598c
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-04-19 22:12:57 +0300

    Determine Color of a Chessboard Square

---
+++ leetcode1812.cs
@@ -0,0 +1,22 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 1812. Determine Color of a Chessboard Square
+
+DESCRIPTION:
+You are given coordinates, a string that represents the coordinates of a square
+of the chessboard. Below is a chessboard for your reference.
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public bool SquareIsWhite(string coordinates)
+  {
+    return (coordinates[0] + char.GetNumericValue(coordinates[1])) % 2 != 0;
+  }
+
+  public static void Main()
+  {
+  }
+}
