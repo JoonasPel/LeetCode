a609fbbd0518def4c9823096aacb5d6ce50a7e61
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-05-15 21:29:15 +0300

    Build Array from Permutation

---
+++ leetcode1920.cs
@@ -0,0 +1,34 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 1920. Build Array from Permutation
+
+DESCRIPTION:
+Given a zero-based permutation nums (0-indexed), build an array ans of the same
+length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it
+
+A zero-based permutation nums is an array of distinct integers from 0 to
+nums.length - 1 (inclusive).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public int[] BuildArray(int[] nums)
+  {
+    int[] ans = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+      ans[i] = nums[nums[i]];
+    }
+    return ans;
+  }
+
+  public static void Main()
+  {
+  }
+}
