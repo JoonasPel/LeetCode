d1091d88e869e115d6df9289ae76e7c0087d4f33
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-05-20 23:26:52 +0300

    Find the Integer Added to Array I

---
+++ leetcode3131.cs
@@ -0,0 +1,25 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 3131. Find the Integer Added to Array I
+
+DESCRIPTION:
+You are given two arrays of equal length, nums1 and nums2.
+
+Each element in nums1 has been increased (or decreased in the case of negative)
+by an integer, represented by the variable x.
+
+As a result, nums1 becomes equal to nums2. Two arrays are considered equal when
+they contain the same integers with the same frequencies.
+
+Return the integer x.
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public int AddedInteger(int[] nums1, int[] nums2)
+  {
+    return nums2.Max() - nums1.Max();
+  }
+}
