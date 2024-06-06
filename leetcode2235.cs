0e7c2cfcef9531aca324326294e1f4b0cd564a45
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-06-06 19:46:55 +0300

    Add Two Integers

---
+++ leetcode2235.cs
@@ -0,0 +1,22 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2235. Add Two Integers
+
+DESCRIPTION:
+Given two integers num1 and num2, return the sum of the two integers. 
+
+INTUITION/APPROACH:
+XD
+*/
+
+public class Solution
+{
+  public int Sum(int num1, int num2)
+  {
+    return num1 + num2;
+  }
+
+  public static void Main()
+  {
+  }
+}
