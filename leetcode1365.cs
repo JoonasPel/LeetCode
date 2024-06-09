c0f32a6ce47801e262a402e84a0e6e417fb73229
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-06-09 11:49:20 +0300

    How Many Numbers Are Smaller Than the Current Number

---
+++ leetcode1365.cs
@@ -0,0 +1,43 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 1365. How Many Numbers Are Smaller Than the Current Number
+
+DESCRIPTION:
+Given the array nums, for each nums[i] find out how many numbers in the array
+are smaller than it. That is, for each nums[i] you have to count the number of
+valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public int[] SmallerNumbersThanCurrent(int[] nums)
+  {
+    int[] original = new int[nums.Length];
+    Array.Copy(nums, original, nums.Length);
+    Array.Sort(nums);
+    Dictionary<int, int> smallerCounts = new();
+    smallerCounts[nums[0]] = 0;
+    for (int i = 1; i < nums.Length; i++)
+    {
+      if (nums[i] > nums[i - 1])
+      {
+        smallerCounts[nums[i]] = i;
+      }
+    }
+    for (int i = 0; i < nums.Length; i++)
+    {
+      original[i] = smallerCounts[original[i]];
+    }
+    return original;
+  }
+
+  public static void Main()
+  {
+    Solution app = new();
+    app.SmallerNumbersThanCurrent([8, 1, 2, 2, 3]);
+  }
+}
