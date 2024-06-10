51fc5fb18733cb49362c8b7c937cff27f3d2d819
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-06-10 22:39:49 +0300

    Difference Between Element Sum and Digit Sum of an Array

---
+++ leetcode2535.cs
@@ -0,0 +1,50 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2535. Difference Between Element Sum and Digit Sum of an Array
+
+DESCRIPTION:
+You are given a positive integer array nums.
+
+    The element sum is the sum of all the elements in nums.
+    The digit sum is the sum of all the digits (not necessarily distinct) that
+    appear in nums.
+
+Return the absolute difference between the element sum and digit sum of nums.
+
+Note that the absolute difference between two integers x and y is defined as
+|x - y|.
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public int DifferenceOfSum(int[] nums)
+  {
+    int digitSum = 0;
+    int elementSum = 0;
+    foreach (int num in nums)
+    {
+      elementSum += num;
+      digitSum += CalcDigitSum(num);
+    }
+    return Math.Abs(elementSum - digitSum);
+
+    int CalcDigitSum(int number)
+    {
+      int res = 0;
+      while (number > 0)
+      {
+        res += number % 10;
+        number /= 10;
+      }
+      return res;
+    }
+  }
+
+  public static void Main()
+  {
+    Solution app = new();
+    app.DifferenceOfSum([1, 15, 6, 3]);
+  }
+}
