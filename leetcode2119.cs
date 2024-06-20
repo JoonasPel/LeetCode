6d930d3ac27d947ac316042c2edf444a0e503de3
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-06-20 22:54:50 +0300

    A Number After a Double Reversal

---
+++ leetcode2119.cs
@@ -0,0 +1,27 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2119. A Number After a Double Reversal
+
+DESCRIPTION:
+Reversing an integer means to reverse all its digits.
+
+    For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the
+    leading zeros are not retained.
+
+Given an integer num, reverse num to get reversed1, then reverse reversed1 to
+get reversed2. Return true if reversed2 equals num. Otherwise return false.
+
+INTUITION/APPROACH:
+*/
+
+public class Solution
+{
+  public bool IsSameAfterReversals(int num)
+  {
+    return num % 10 != 0 || num == 0;
+  }
+
+  public static void Main()
+  {
+  }
+}
