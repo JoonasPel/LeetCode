fcfa2930873bf1ee17774a2b00edc27c6c7c70c9
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-07-01 20:38:51 +0300

    Check if All A's Appears Before All B's

---
+++ leetcode2124.cs
@@ -0,0 +1,34 @@
+﻿/*
+DIFFICULTY: Easy
+QUESTION: 2124. Check if All A's Appears Before All B's
+
+DESCRIPTION:
+Given a string s consisting of only the characters 'a' and 'b', return true if
+every 'a' appears before every 'b' in the string. Otherwise, return false.
+
+INTUITION/APPROACH:
+Start from index 0 and keep track if we have seen 'b'. If we find 'a' and have
+seen 'b', return false.
+
+TIME COMPLEXITY: O(n)
+
+THOUGHTS:
+*/
+
+public class Solution
+{
+  public bool CheckString(string s)
+  {
+    bool seenB = false;
+    foreach (char c in s)
+    {
+      if (c == 'a' && seenB) return false;
+      else if (c == 'b') seenB = true;
+    }
+    return true;
+  }
+
+  public static void Main()
+  {
+  }
+}
